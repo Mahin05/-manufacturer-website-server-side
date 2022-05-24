ec10dc739b8db85cad41b3909bb0ca8939ae4bab
Mahin05 <mahin.islam05@gmail.com>
2022-05-25 01:32:20 +0600

    user info add

--- index.js
+++ index.js
@@ -13,13 +13,27 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kx9ii.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
 async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db("products").collection('tools');
         const reviewCollection = client.db("products").collection('reviews');
         const userCollection = client.db("products").collection('users');
+        const userInfoCollection = client.db("products").collection('usersInfo');
 
         app.get('/tools', async (req, res) => {
             const query = {};
@@ -36,12 +50,25 @@ async function run() {
             const updateDoc = {
               $set: user,
             };
-      
             const result = await userCollection.updateOne(filter, updateDoc, options);
             var token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             res.send({ result, token });
           })
-        app.get('/purchase/:id', async (req, res) => {
+
+        app.post('/userInfo/:email',async(req,res)=>{
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email};
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: user,
+            };
+            const result = await userInfoCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
+
+        app.get('/purchase/:id', verifyJWT,async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const tool = await toolsCollection.findOne(query);
