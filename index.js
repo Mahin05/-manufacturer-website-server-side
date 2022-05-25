8ab3769ae40a1cb751682989064e9c0364bfdfa1
Mahin05 <mahin.islam05@gmail.com>
2022-05-25 18:21:26 +0600

    user get

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
- 
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -14,90 +14,111 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kx9ii.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      return res.status(401).send({ message: 'UnAuthorized access' });
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-      if (err) {
-        return res.status(403).send({ message: 'Forbidden access' })
-      }
-      req.decoded = decoded;
-      next();
-    });
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
   }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 async function run() {
-    try {
-        await client.connect();
-        const toolsCollection = client.db("products").collection('tools');
-        const reviewCollection = client.db("products").collection('reviews');
-        const userCollection = client.db("products").collection('users');
-        const userInfoCollection = client.db("products").collection('usersInfo');
+  try {
+    await client.connect();
+    const toolsCollection = client.db("products").collection('tools');
+    const reviewCollection = client.db("products").collection('reviews');
+    const userCollection = client.db("products").collection('users');
+    const userInfoCollection = client.db("products").collection('usersInfo');
 
-        app.get('/tools', async (req, res) => {
-            const query = {};
-            const cursor = toolsCollection.find(query);
-            const tools = await cursor.limit(6).toArray();
-            res.send(tools);
-        });
+    app.get('/tools', async (req, res) => {
+      const query = {};
+      const cursor = toolsCollection.find(query);
+      const tools = await cursor.limit(6).toArray();
+      res.send(tools);
+    });
 
-        app.put('/user/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = req.body;
-            const filter = { email: email };
-            const options = { upsert: true };
-            const updateDoc = {
-              $set: user,
-            };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
-            var token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            res.send({ result, token });
-          })
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      var token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ result, token });
+    })
 
-        app.post('/userInfo/:email',async(req,res)=>{
-            const email = req.params.email;
-            const user = req.body;
-            const filter = { email: email};
-            const options = { upsert: true };
-            const updateDoc = {
-              $set: user,
-            };
-            const result = await userInfoCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
-        })
+    app.post('/userInfo/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userInfoCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+    // app.put('/userInfo/:email', async (req, res) => {
+    //   const email = req.params.email;
+    //   const user = req.body;
+    //   const filter = { email: email };
+    //   const options = { upsert: true };
+    //   const updateDoc = {
+    //     $set: user,
+    //   };
+    //   const result = await userInfoCollection.updateOne(filter, updateDoc, options);
+    //   res.send(result);
+    // })
+    app.get('/user',verifyJWT,async(req,res)=>{
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    })
 
+    app.get('/userInfo/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = {email:email};
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    })
 
-        app.get('/purchase/:id', verifyJWT,async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const tool = await toolsCollection.findOne(query);
-            res.send(tool);
-        });
+    app.get('/purchase/:id', verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    });
 
-        app.post('/review', async (req, res) => {
-            const reviews = req.body;
-            const result = await reviewCollection.insertOne(reviews);
-            res.send(result);
-        })
-        app.get('/reviews', async (req, res) => {
-            const query = {};
-            const cursor = reviewCollection.find(query);
-            const reviews = await cursor.toArray();
-            res.send(reviews);
-        })
+    app.post('/review', async (req, res) => {
+      const reviews = req.body;
+      const result = await reviewCollection.insertOne(reviews);
+      res.send(result);
+    })
+    app.get('/reviews', async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    })
 
-    }
-    finally {
-        // await client.close();
-    }
+  }
+  finally {
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
- 
+
 app.get('/', (req, res) => {
-   res.send('Server is running properly!')
+  res.send('Server is running properly!')
 })
-  app.listen(port, () => {
- console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
