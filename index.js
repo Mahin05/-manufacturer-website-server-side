d1027fbbd4be6d23ad93bd527138c82926df50a0
Mahin05 <mahin.islam05@gmail.com>
2022-05-26 21:18:39 +0600

    updated api

--- index.js
+++ index.js
@@ -120,6 +120,13 @@ async function run() {
       const result = await orderCollection.deleteOne(query);
       res.send(result);
     })
+    app.delete('/tool/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await toolsCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.delete('/cancel/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
