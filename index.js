b15c73bc7772b8d7f8874094a59eacb58e0b8210
Mahin05 <mahin.islam05@gmail.com>
2022-05-26 20:52:14 +0600

    jwt added

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
 // middleware
@@ -35,11 +36,35 @@ async function run() {
     const userCollection = client.db("products").collection('users');
     const userInfoCollection = client.db("products").collection('usersInfo');
     const orderCollection = client.db("products").collection('orders');
+    const paymentCollection = client.db("products").collection('payments');
+
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        next();
+      }
+      else {
+        res.status(403).send({ message: 'forbidden' });
+      }
+    }
+
+    app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+      const toolName = req.body;
+      const pricePerunit = toolName.pricePerunit;
+      const amount = pricePerunit*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency: 'usd',
+        payment_method_types:['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+      });
 
     app.get('/tools', async (req, res) => {
       const query = {};
       const cursor = toolsCollection.find(query);
-      const tools = await cursor.limit(6).toArray();
+      const tools = await cursor.toArray();
       res.send(tools);
     });
 
@@ -49,28 +74,87 @@ async function run() {
       res.send(result);
     })
 
-     // individual Order
-     app.get('/order',async(req,res)=>{
-        const email = req.query.email;
-        const query = {email:email};
-        const bookings = await orderCollection.find(query).toArray();
-        res.send(bookings);
-     })
+    // individual Order
+    app.get('/booking',async(req,res)=>{
+      const email= req.query.email;
+      const query = {email:email};
+      const bookings = await orderCollection.find(query).toArray();
+      res.send(bookings)
+    })
 
-     // get edit user
-     app.get('/booking',async(req,res)=>{
-       const email = req.query.email;
-       const query = {email:email};
-       const bookings = await userInfoCollection.find(query).toArray();
-       res.send(bookings);
-     })
+
+    app.get('/order/:id',verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await orderCollection.findOne(query);
+      res.send(order);
+    })
+
+    // get edit user
+    app.get('/booking', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await userInfoCollection.find(query).toArray();
+      res.send(bookings);
+    })
+
+    //  update fields api
+    app.put('/update', async (req, res) => {
+      const email = req.params.email;
+      const updatedInfo = req.body;
+      const filter = { email: email };
+      const options = { upsert: true }
+      const updatedDoc = {
+        $set: {
+          quantity: updatedInfo.quantity
+        }
+      };
+      const result = await userInfoCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    })
+
+    // delete
+    app.delete('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    })
+    app.delete('/cancel/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    app.put('/shipped/:id', async (req, res) => {
+      const id = req.params.id;
+      const shipped = req.body;
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true }
+      const updatedDoc = {
+        $set: {
+          shipped: shipped
+        }
+      };
+      const result = await userInfoCollection.updateOne(query, updatedDoc, options);
+      res.send(result);
+    })
+
+    app.get('/manageInventory', async (req, res) => {
+      const query = {};
+      const cursor = orderCollection.find(query);
+      const inventories = await cursor.toArray();
+      res.send(inventories);
+    });
 
 
     app.get('/admin/:email', async (req, res) => {
       const email = req.params.email;
-      const user = await userCollection.findOne({ email: email })
+      const user = await userCollection.findOne({ email: email });
       const isAdmin = user.role === 'admin';
-      res.send({ admin: isAdmin });
+      res.send({ admin: isAdmin })
     })
 
     app.put('/user/:email', async (req, res) => {
@@ -81,25 +165,22 @@ async function run() {
       const updateDoc = {
         $set: user,
       };
+
       const result = await userCollection.updateOne(filter, updateDoc, options);
       var token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
       res.send({ result, token });
     })
-    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+
+
+    app.put('/user/admin/:email', verifyJWT,verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({ email: requester })
-      if (requesterAccount.role === 'admin') {
-        const filter = { email: email };
-        const updateDoc = {
-          $set: { role: 'admin' },
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
-      }
-      else {
-        return res.status(403).send({ message: 'Forbidden access' })
-      }
+
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: 'admin' },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
     })
 
     app.post('/order', async (req, res) => {
@@ -114,17 +195,17 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/user', async (req, res) => {
+    app.get('/user', verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     })
 
-    // app.get('/userInfo/:email', async (req, res) => {
-    //   const email = req.params.email;
-    //   const query = { email: email };
-    //   const tool = await toolsCollection.findOne(query);
-    //   res.send(tool);
-    // })
+    app.get('/userInfo/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    })
 
     app.get('/purchase/:id', async (req, res) => {
       const id = req.params.id;
@@ -143,6 +224,27 @@ async function run() {
       const cursor = reviewCollection.find(query);
       const reviews = await cursor.toArray();
       res.send(reviews);
+    })
+    app.get('/allorders',async(req,res)=>{
+      const query = {};
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.patch('/booking/:id', verifyJWT, async(req, res) =>{
+      const id  = req.params.id;
+      const payment = req.body;
+      const filter = {_id: ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId
+        }
+      }
+
+      const result = await paymentCollection.insertOne(payment);
+      const updatedBooking = await orderCollection.updateOne(filter, updatedDoc);
+      res.send(updatedBooking);
     })
 
   }
