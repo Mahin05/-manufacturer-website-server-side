87d38f0503bf5893be5ee0b917fc096f7df7f5f7
Mahin05 <mahin.islam05@gmail.com>
2023-05-15 21:27:55 +0600

    latest code

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 //const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kx9ii.mongodb.net/?retryWrites=true&w=majority`;
 const uri = "mongodb+srv://admin:L8UDTcBJt4wLkNXD@cluster0.kx9ii.mongodb.net/?retryWrites=true&w=majority";
-
+ 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
